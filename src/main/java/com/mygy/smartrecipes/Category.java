d63ae4f802f88ad55ba2b1cb31f837fad6d2fcc8
Иулиания Жуковец <131142939+Irromord@users.noreply.github.com>
2023-12-21 00:24:40 +0900

    Update Category.java

--- src/main/java/com/mygy/smartrecipes/Category.java
+++ src/main/java/com/mygy/smartrecipes/Category.java
@@ -1,31 +1,39 @@
+// Класс непосредственно самих категорий
+
 package com.mygy.smartrecipes;
 
 import java.io.Serializable;
 import java.util.ArrayList;
 
+// Класс Category, который реализует интерфейс Serializable для возможности сохранения его экземпляров
 public class Category implements Serializable {
-    private String name;
-    private int btnImageRes;
-    private ArrayList<Reciepe> resiepes = new ArrayList<>();
+    private String name; // Имя категории
+    private int btnImageRes; // ID изображения для кнопки
+    private ArrayList<Reciepe> resiepes = new ArrayList<>(); // Список рецептов, принадлежащих к этой категории
 
+    // Конструктор класса Category
     public Category(String name, int btnImageRes) {
-        this.name = name;
-        this.btnImageRes = btnImageRes;
+        this.name = name; // Установка имени категории
+        this.btnImageRes = btnImageRes; // Установка изображения для кнопки
     }
 
+    // Геттер для получения имени категории
     public String getName() {
         return name;
     }
 
+    // Геттер для получения ID изображения кнопки
     public int getBtnImageRes() {
         return btnImageRes;
     }
 
+    // Геттер для получения списка рецептов категории
     public ArrayList<Reciepe> getResiepes() {
         return resiepes;
     }
 
+    // Метод для добавления нового рецепта в список рецептов категории
     public void addReciepe(Reciepe reciepe){
-        resiepes.add(reciepe);
+        resiepes.add(reciepe); // Добавление рецепта в список
     }
 }
