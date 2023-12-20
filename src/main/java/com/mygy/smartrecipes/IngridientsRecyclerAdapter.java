ba319e56caf28f6d17432af2db6fc7276ad41c9b
Иулиания Жуковец <131142939+Irromord@users.noreply.github.com>
2023-12-21 01:30:23 +0900

    Update IngridientsRecyclerAdapter.java

--- src/main/java/com/mygy/smartrecipes/IngridientsRecyclerAdapter.java
+++ src/main/java/com/mygy/smartrecipes/IngridientsRecyclerAdapter.java
@@ -11,82 +11,57 @@ import android.widget.TextView;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
-/**
- * Адаптер для RecyclerView, который управляет отображением списка ингредиентов.
- * Этот класс наследуется от RecyclerView.Adapter и предоставляет необходимую
- * связь между данными и представлением элементов в RecyclerView.
- */
+// Адаптер для списка ингредиентов в RecyclerView
 public class IngridientsRecyclerAdapter extends RecyclerView.Adapter<IngridientsRecyclerAdapter.ViewHolder> {
 
-    // Переменная для инфлейтинга (создания) View из XML-макета
+    // Для создания новых View элементов
     private final LayoutInflater inflater;
 
-    // Список объектов Ingridient, который содержит данные, отображаемые в RecyclerView
+    // Список ингредиентов для отображения
     private final List<Ingridient> ingridients;
 
-    /**
-     * Конструктор адаптера.
-     * @param context Контекст, используемый для инфлейтинга макетов.
-     * @param ingridients Список данных, которые необходимо отобразить в RecyclerView.
-     */
+    // Конструктор: принимает контекст и список ингредиентов
     public IngridientsRecyclerAdapter(Context context, List<Ingridient> ingridients) {
         this.ingridients = ingridients;
         this.inflater = LayoutInflater.from(context);
     }
 
-    /**
-     * Создает новые View-элементы для каждого элемента в RecyclerView.
-     * @param parent ViewGroup, в который будет добавлен новый View.
-     * @param viewType Тип представления элемента.
-     * @return ViewHolder с созданным View.
-     */
+    // Создает и возвращает ViewHolder для каждого элемента в списке
     @Override
     public IngridientsRecyclerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = inflater.inflate(R.layout.ingridient_item, parent, false);
         return new IngridientsRecyclerAdapter.ViewHolder(view);
     }
 
-    /**
-     * Привязывает данные к View-элементам.
-     * @param holder ViewHolder, содержащий View-элементы.
-     * @param position Позиция элемента в RecyclerView.
-     */
+    // Заполняет ViewHolder данными ингредиента
     @Override
     public void onBindViewHolder(IngridientsRecyclerAdapter.ViewHolder holder, int position) {
-        Ingridient ingridient = ingridients.get(position); // Получение ингредиента по позиции
-
-        // Назначение текста кнопке и обработка событий нажатия на кнопку
+        Ingridient ingridient = ingridients.get(position);
         holder.btn.setText(ingridient.getName());
         holder.btn.setOnClickListener(v -> {
-            // Обработка выбора/снятия выбора ингредиента
+            // Меняет видимость иконки и обновляет список выбранных ингредиентов
             if(Ingridient.selectedIngridient.contains(ingridient)){
                 holder.selectIco.setVisibility(View.INVISIBLE);
                 Ingridient.selectedIngridient.remove(ingridient);
-            }else{
+            } else {
                 holder.selectIco.setVisibility(View.VISIBLE);
                 Ingridient.selectedIngridient.add(ingridient);
             }
         });
     }
 
-    /**
-     * Возвращает количество элементов в списке ингредиентов.
-     * @return Размер списка ingridients.
-     */
+    // Возвращает количество ингредиентов в списке
     @Override
     public int getItemCount() {
         return ingridients.size();
     }
 
-    /**
-     * Внутренний класс ViewHolder для управления View-элементами в каждом элементе списка RecyclerView.
-     */
+    // Внутренний класс для хранения элементов интерфейса каждого элемента в списке
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        // TextView для отображения названия ингредиента и ImageView для отображения иконки выбора
-        final TextView btn;
-        final ImageView selectIco;
+        final TextView btn; // Кнопка для названия ингредиента
+        final ImageView selectIco; // Иконка для выбора ингредиента
 
-        // Конструктор ViewHolder
+        // Конструктор для инициализации элементов интерфейса
         ViewHolder(View view){
             super(view);
             btn = view.findViewById(R.id.ingridient_btn);
