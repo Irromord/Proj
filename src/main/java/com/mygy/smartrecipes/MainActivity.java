956d1a793e62a64a49771c9884eaa7c1de30f590
Иулиания Жуковец <131142939+Irromord@users.noreply.github.com>
2023-12-21 01:10:40 +0900

    Update MainActivity.java

--- src/main/java/com/mygy/smartrecipes/MainActivity.java
+++ src/main/java/com/mygy/smartrecipes/MainActivity.java
@@ -1,3 +1,5 @@
+//вступительный экран с кнопкной "начать"
+
 package com.mygy.smartrecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,16 +8,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+// Основной класс Activity для приложения.
 public class MainActivity extends AppCompatActivity {
 
+    // Метод onCreate вызывается при создании Activity.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Установка пользовательского интерфейса для этой Activity из XML-макета.
         setContentView(R.layout.activity_main);
 
+        // Находим кнопку запуска по её ID в XML-макете.
         Button startBtn = findViewById(R.id.main_startBtn);
+
+        // Устанавливаем обработчик нажатия на кнопку запуска.
         startBtn.setOnClickListener(v -> {
+            // Создание нового Intent (намерения), механизма для для перехода от MainActivity к SelectCategoryActivity.
             Intent intent = new Intent(this, SelectCategoryActivity.class);
+
+            // Запуск SelectCategoryActivity.
             startActivity(intent);
         });
     }
